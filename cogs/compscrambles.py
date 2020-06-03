0611047fabf05e28579b5b295ca846cf9e39a286
Veni <63829007+midnightcuberx@users.noreply.github.com>
2020-06-03 19:55:42 +1200

    Update compscrambles.py

--- cogs/compscrambles.py
+++ cogs/compscrambles.py
@@ -16,10 +16,17 @@ class Comp(commands.Cog):
   def __init__(self,bot):
     self.bot=bot
 
-
+  @commands.command()
+  @commands.has_permissions(manage_messages=True)
+  async def start(self,ctx):
+    coll=db["mode"]
+    coll.update_one({"_id":ctx.guild.id},{"$set":{"mode":"on"}})
+    await ctx.send("Successfully started the weekly comp")
   @commands.command()
   @commands.has_permissions(manage_messages=True)
   async def comp(self,ctx):
+    coll=db["mode"]
+    coll.update_one({"_id":ctx.guild.id},{"$set":{"mode":"on"}})
     await ctx.send("This may take a while to generate. Please give it a minute")
     scrambles=[]
     results=collection.find({"_id":ctx.guild.id})
@@ -39,7 +46,7 @@ class Comp(commands.Cog):
     scrambles=[]
     if config['4x4']['mode']=="on":
       for i in range(5):
-        client1= pymongo.MongoClient(insert code)
+        client1= pymongo.MongoClient("mongodb+srv://midnightcuberx:Annarcher811@cluster0-epubu.mongodb.net/test?retryWrites=true&w=majority")
         db2=client1["scrambles"]
         coll=db2["4x4"]
         results=coll.find({})
@@ -126,12 +133,56 @@ class Comp(commands.Cog):
         scrambles.append(scram)
       await ctx.send(f"Clock:\n-\n> 1) {scrambles[0]}\n-\n> 2) {scrambles[1]}\n-\n> 3) {scrambles[2]}\n-\n> 4) {scrambles[3]}\n-\n> 5) {scrambles[4]}")
     scrambles=[]
-    if config['4x4']['mode']=="on":
+    if config['megaminx']['mode']=="on":
       for i in range(5):
         scram=side.get_mega()
         scrambles.append(scram)
       await ctx.send(f"Megaminx:\n-\n> 1) {scrambles[0]}\n-\n> 2) {scrambles[1]}\n-\n> 3) {scrambles[2]}\n-\n> 4) {scrambles[3]}\n-\n> 5) {scrambles[4]}")
     await ctx.send("All done.")
+    scrambles=[]
+    if config["fmc"]["mode"]=="on":
+      for i in range(5):
+        a=scrambler333.get_WCA_scramble().split()
+        list1=["F","F2","F'","B'","B2","B"]
+        list2=["R2","R","R'","L","L'","L2"]
+        list3=["F","F2","F'"]
+        list4=["R2","R","R'"]
+        print(a[-1])
+        print(a)
+        times=0
+        while (a[0] in list1 and a[1] in list1) or (a[0] in list3) or (a[-1] in list2 and a[-2] in list2) or (a[-1] in list4):
+          if times>3:
+            a=scrambler333.get_WCA_scramble().split()
+            times=0
+
+          if a[0] in list3:
+            a.remove(a[0])
+            times+=1
+
+          if (a[0] in list1 and a[1] in list1):
+            if a[0] not in list3:
+              a.remove(a[1])
+              times+=1
+            else:
+              a.remove(a[0])
+              times+=1
+          
+          if a[-1] in list4:
+            a.remove(a[-1])
+            times+=1
+          
+          if (a[-1] in list2 and a[-2] in list2):
+            if a[-1] not in list4:
+              a.remove(a[-2])
+              times+=1
+            else:
+              a.remove(a[-1])
+              times+=1
+          print(times)
+        scrambles.append("R' U' F " + " ".join(a) + " R' U' F")
+      await ctx.send(f"Megaminx:\n-\n> 1) {scrambles[0]}\n-\n> 2) {scrambles[1]}\n-\n> 3) {scrambles[2]}\n-\n> 4) {scrambles[3]}\n-\n> 5) {scrambles[4]}")
+    await ctx.send("All done")
+  
 
 
 
