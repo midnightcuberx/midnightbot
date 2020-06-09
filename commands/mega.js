aa798e0578a16d79465a34673ecf2dc473bec492
Veni <63829007+midnightcuberx@users.noreply.github.com>
2020-06-09 22:13:42 +1200

    Create mega.js

---
+++ commands/mega.js
@@ -0,0 +1,27 @@
+const scrambler = require('scrambler-util');
+module.exports = {
+	name: 'mega',
+  aliases: ['megaminx'],
+	execute(client,message, args) {
+    if(args[0]===undefined){
+      args[0]=1
+    }
+    let scramlen=parseInt(args[0])
+    if(scramlen>5){
+      scramlen=5
+      let scrambleArr = scrambler("mega",scramlen ); 
+      for (i = 0; i < scramlen; i++){
+        scrambleArr[i]=(i+1)+") "+scrambleArr[i]
+      }
+      message.channel.send(scrambleArr.join("\n"));
+      message.channel.send("5 scrambles is the maximum amount.")
+    }
+    else{
+      let scrambleArr = scrambler("mega",scramlen ); 
+      for (i = 0; i < scramlen; i++){
+        scrambleArr[i]=(i+1)+") "+scrambleArr[i]
+      }
+      message.channel.send(scrambleArr.join("\n"));
+    }
+	},
+};
