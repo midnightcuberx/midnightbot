9ebb868b5783e704fc647496fe8a7670d3c9dbd5
Veni <63829007+midnightcuberx@users.noreply.github.com>
2020-06-09 21:58:19 +1200

    Create sq1.js

---
+++ commands/sq1.js
@@ -0,0 +1,27 @@
+const scrambler = require('scrambler-util');
+module.exports = {
+	name: 'squan',
+  aliases: ['sq1','square-1'],
+	execute(client,message, args) {
+    if(args[0]===undefined){
+      args[0]=1
+    }
+    let scramlen=parseInt(args[0])
+    if(scramlen>12){
+      scramlen=12
+      let scrambleArr = scrambler("sq1",scramlen ); 
+      for (i = 0; i < scramlen; i++){
+        scrambleArr[i]=(i+1)+") "+scrambleArr[i]
+      }
+      message.channel.send(scrambleArr.join("\n"));
+      message.channel.send("12 scrambles is the maximum amount.")
+    }
+    else{
+      let scrambleArr = scrambler("sq1",scramlen ); 
+      for (i = 0; i < scramlen; i++){
+        scrambleArr[i]=(i+1)+") "+scrambleArr[i]
+      }
+      message.channel.send(scrambleArr.join("\n"));
+    }
+	},
+};
