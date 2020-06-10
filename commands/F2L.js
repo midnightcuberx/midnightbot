1162bc363bbcea7da171af88658450c94d9f90df
Veni <63829007+midnightcuberx@users.noreply.github.com>
2020-06-11 07:04:31 +1200

    Create F2L.js

---
+++ commands/F2L.js
@@ -0,0 +1,31 @@
+const scramble_333=require("./lib/scramble_333_edit");
+module.exports = {
+	name: 'f2l',
+  aliases: [],
+	execute(client,message, args) {
+    if(args[0]===undefined){
+      args[0]=1
+    }
+    let scrambleArr = [];
+    let scramble="" 
+    let scramlen=parseInt(args[0])
+    if(scramlen>12){
+      scramlen=12
+      for (i = 0; i < scramlen; i++){
+        scramble=scramble_333.getF2LScramble()
+        scrambleArr.push(scramble)
+        scrambleArr[i]=(i+1)+") "+scrambleArr[i]
+      }
+      message.channel.send(scrambleArr.join("\n"));
+      message.channel.send("12 scrambles is the maximum amount.")
+    }
+    else{
+      for (i = 0; i < scramlen; i++){
+        scramble=scramble_333.getF2LScramble()
+        scrambleArr.push(scramble)
+        scrambleArr[i]=(i+1)+") "+scrambleArr[i]
+      }
+      message.channel.send(scrambleArr.join("\n"));
+    }
+	},
+};
