f7e64aaed04608e0a6026a331c18adbd677a741c
Veni <63829007+midnightcuberx@users.noreply.github.com>
2020-06-10 21:19:22 +1200

    Update 6x6.js

--- commands/6x6.js
+++ commands/6x6.js
@@ -1,4 +1,4 @@
-const scrambler = require('scrambler-util');
+var megaScrambler = require("./lib/megascramble");
 module.exports = {
 	name: '6x6',
   aliases: ['6'],
@@ -6,20 +6,25 @@ module.exports = {
     if(args[0]===undefined){
       args[0]=1
     }
+    let scrambleArr=[]
+    let scramble=""
     let scramlen=parseInt(args[0])
     if(scramlen>5){
-      scramlen=5
-      let scrambleArr = scrambler("666",scramlen ); 
+      scramlen=5 
       for (i = 0; i < scramlen; i++){
+        scramble=megaScrambler.get666WCAScramble(80)
+        scrambleArr.push(scramble)
         scrambleArr[i]=(i+1)+") "+scrambleArr[i]
       }
       message.channel.send(scrambleArr.join("\n"));
       message.channel.send("5 scrambles is the maximum amount.")
     }
     else{
-      let scrambleArr = scrambler("666",scramlen ); 
       for (i = 0; i < scramlen; i++){
+        scramble=megaScrambler.get666WCAScramble(80)
+        scrambleArr.push(scramble)
         scrambleArr[i]=(i+1)+") "+scrambleArr[i]
+
       }
       message.channel.send(scrambleArr.join("\n"));
     }
